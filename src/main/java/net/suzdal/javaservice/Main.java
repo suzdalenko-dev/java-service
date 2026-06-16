72cb5d190843f315b075c34daa0a78a2c4469765
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2026-06-16 09:01:23 +0200

    su

--- src/main/java/net/suzdal/javaservice/Main.java
+++ src/main/java/net/suzdal/javaservice/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaServiceApplication {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(JavaServiceApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
